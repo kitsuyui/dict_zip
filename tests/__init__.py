4c5e65302d8287274ccb06d5f042f72d45e3d91c
kitsuyui <kitsuyui@kitsuyui.com>
2022-05-08 16:33:27 +0900

    Preserve key order
    From Python 3.7, dict became order preserving.
    dict_zip and dict_zip_longest should prefer this feature.

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,20 +4,43 @@ from dict_zip import dict_zip, dict_zip_longest
 
 
 class TestDictZip(unittest.TestCase):
-
     def test_basis(self) -> None:
-        d1 = {'a': 1, 'b': 2, 'c': 3}
-        d2 = {'a': 4, 'b': 5}
+        d1 = {"a": 1, "b": 2, "c": 3}
+        d2 = {"a": 4, "b": 5}
 
-        self.assertEqual(dict_zip(d1, d2),
-                         {'a': (1, 4), 'b': (2, 5)})
+        self.assertEqual(dict_zip(d1, d2), {"a": (1, 4), "b": (2, 5)})
 
-        self.assertEqual(dict_zip_longest(d1, d2),
-                         {'a': (1, 4), 'b': (2, 5), 'c': (3, None)})
+        self.assertEqual(
+            dict_zip_longest(d1, d2),
+            {"a": (1, 4), "b": (2, 5), "c": (3, None)}
+        )
 
     def test_fill_value(self) -> None:
-        d1 = {'a': 1, 'b': 2, 'c': 3}
-        d2 = {'a': 4, 'b': 5}
+        d1 = {"a": 1, "b": 2, "c": 3}
+        d2 = {"a": 4, "b": 5}
 
-        self.assertEqual(dict_zip_longest(d1, d2, fillvalue=10),
-                         {'a': (1, 4), 'b': (2, 5), 'c': (3, 10)})
+        self.assertEqual(
+            dict_zip_longest(d1, d2, fillvalue=10),
+            {"a": (1, 4), "b": (2, 5), "c": (3, 10)},
+        )
+
+    def test_key_order(self) -> None:
+        d1 = {"a": 1, "b": 2}
+        d2 = {"b": 4, "c": 2, "a": 5}
+        self.assertEqual(
+            list(dict_zip(d1, d2).keys()),
+            ["a", "b"],
+        )
+        self.assertEqual(
+            list(dict_zip_longest(d1, d2).keys()),
+            ["a", "b", "c"],
+        )
+
+        self.assertEqual(
+            list(dict_zip(d2, d1).keys()),
+            ["b", "a"],
+        )
+        self.assertEqual(
+            list(dict_zip_longest(d2, d1).keys()),
+            ["b", "c", "a"],
+        )
