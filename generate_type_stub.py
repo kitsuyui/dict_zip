345b1ebef931752407f8473113c73a246d8d933a
kitsuyui <kitsuyui@kitsuyui.com>
2022-05-08 14:52:12 +0900

    Update type stubs
    - [x] Accept more arguments
    - [x] Fix import order by isort
    - [x] Remove unnecessary Optional Fill value.

--- generate_type_stub.py
+++ generate_type_stub.py
@@ -1,4 +1,4 @@
-NUMBER_OF_ITERATORS = 6
+NUMBER_OF_ITERATORS = 10
 
 print("""\
 from typing import (
@@ -18,6 +18,7 @@ for i in range(1, NUMBER_OF_ITERATORS):
     print(f"""_T{i} = TypeVar('_T{i}')""")
 print()
 print()
+print("# type stubs for dict_zip")
 for i in range(1, NUMBER_OF_ITERATORS):
     arguments = ', '.join(f'dict{j}: Dict[K, _T{j}]' for j in range(1, 1 + i))
     return_tuple_types = ', '.join(f'_T{j}' for j in range(1, 1 + i))
@@ -27,12 +28,20 @@ for i in range(1, NUMBER_OF_ITERATORS):
 def dict_zip({arguments}) -> {returns}: ...""")
 print()
 print()
+print("# type stubs for dict_zip_longest")
 for i in range(1, NUMBER_OF_ITERATORS):
     arguments = ', '.join(f'dict{j}: Dict[K, _T{j}]' for j in range(1, 1 + i))
-    return_tuple_types = ', '.join(f'Union[_T{j}, Optional[Fill]]'
+    return_tuple_types = ', '.join(f'Optional[_T{j}]'
+                                   for j in range(1, 1 + i))
+    returns = f'Dict[K, Tuple[{return_tuple_types}]]'
+    print(f"""\
+@overload
+def dict_zip_longest({arguments}) -> {returns}: ...""")
+    return_tuple_types = ', '.join(f'Union[_T{j}, Fill]'
                                    for j in range(1, 1 + i))
     returns = f'Dict[K, Tuple[{return_tuple_types}]]'
     print(f"""\
 @overload
 def dict_zip_longest({arguments},
-                     fillvalue: Optional[Fill] = None) -> {returns}: ...""")
+                     *,
+                     fillvalue: Fill = ...) -> {returns}: ...""")
