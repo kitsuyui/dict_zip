4bf775fc7d33c1eec34e05d66292193d4c95baa3
kitsuyui <kitsuyui@kitsuyui.com>
2023-07-23 20:13:59 +0900

    Update docs
    - Add module level doc comments

--- dict_zip/__init__.py
+++ dict_zip/__init__.py
@@ -1,3 +1,17 @@
+"""dict_zip
+
+This module provides a function that concatenates dictionaries.
+Like the zip function for lists, it concatenates dictionaries.
+
+Example:
+    >>> from dict_zip import dict_zip
+    >>> dict_zip({'a': 1, 'b': 2}, {'a': 3, 'b': 4})
+    {'a': (1, 3), 'b': (2, 4)}
+
+    >>> from dict_zip import dict_zip_longest
+    >>> dict_zip_longest({'a': 1, 'b': 2, 'c': 4}, {'a': 3, 'b': 4})
+    {'a': (1, 3), 'b': (2, 4), 'c': (4, None)}
+"""
 import functools
 
 
